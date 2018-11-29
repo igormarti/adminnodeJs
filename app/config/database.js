543b4a1f90b4a6e6fc45b3ede64682bc427d3189
Igor Martins <igorm573@gmail.com>
2018-11-29 10:42:48 -0300

    File of data base was configured

--- app/config/database.js
+++ app/config/database.js
@@ -4,10 +4,10 @@ var conn;
     exports.conn =  function(){
             
                 conn = mysql.createConnection({
-                    host:'',
-                    user:'',
-                    password:'',
-                    database:'',
+                    host:'sql10.freemysqlhosting.net',
+                    user:'sql10267608',
+                    password:'415867Cgw5',
+                    database:'sql10267608',
                     port:3306
                 });
             
